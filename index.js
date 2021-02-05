9dd47bd63d20cb22cb0bb2e6b7d8369818201c35
Brian Ruff <briananthonyruff@gmail.com>
2021-02-05 09:38:58 -0500

    updated code to show user high score and user who set it, and the code for posting scores after the game reaches 100 dots

--- index.js
+++ index.js
@@ -1,105 +1,109 @@
-const bg = document.querySelector("#background");
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('https://boiling-citadel-90849.herokuapp.com/score') 
+      .then(response => response.json()) 
+      .then(data => {
+        document.querySelector("#highScore > h2").innerHTML += " " + data[0].score + "<br> Set by: " + data[0].username;
+  });  
 
-let count = 0;
-let countClick = 0;
-let dotTime;
 
-function clearDots(ele) {
-  const deleteDotsInterval = setInterval(() => {
-    if (ele.parentNode) {
-      ele.parentNode.removeChild(ele);
-    }
-    clearInterval(deleteDotsInterval);
-  }, dotTime);
-}
+  let countClick = 0;
+  let dotTime;
+  let username = document.querySelector("#username");
 
-function moreDots() {
-  for (let i = 0; i < 5; i++) {
-    let dot = document.createElement("div");
-    dot.style.position = "absolute";
-    dot.classList.add("dot");
-    let bottomPositon = Math.floor(
-      Math.min(
-        Math.random() *
-          document.querySelector("#background").getBoundingClientRect().bottom,
-        document.querySelector("#background").getBoundingClientRect().bottom -
-          175
-      )
-    );
-    let rightPosition = Math.min(
-      Math.random() *
-        document.querySelector("#background").getBoundingClientRect().right,
-      document.querySelector("#background").getBoundingClientRect().right - 50
-    );
-    dot.style.bottom = bottomPositon + "px";
-    dot.style.left = rightPosition + "px";
-    document
-      .querySelector("#background")
-      .insertAdjacentElement("afterbegin", dot);
-  }
-  document.querySelectorAll(".dot").forEach(ele => {
-    ele.addEventListener("click", e => {
-      e.target.parentNode.removeChild(e.target);
-    });
-    clearDots(ele);
-  });
-}
-
-function handleDotsOnInteraction(e) {
-  if (e.target.className === "dot") {
-    document.querySelector("#countClick").innerText = countClick += 1;
-  }
-}
-
-document.addEventListener("click", e => handleDotsOnInteraction(e));
-
-function playGame() {
-  let mode = document.querySelectorAll("input[name=mode]:checked")[0];
-  document.querySelector("#countDown").innerText = mode.value;
-  console.log(mode.id);
-  if (mode.id === "easy") {
-    dotTime = 10000;
-  } else if (mode.id === "medium") {
-    dotTime = 7000;
-  } else if (mode.id === "hard") {
-    dotTime = 5000;
-  } else {
-    dotTime = 3000;
-  }
-
-  const countDownTimer = setInterval(() => {
-    document.querySelector("#countDown").innerText =
-      Number(document.querySelector("#countDown").innerText) - 1;
-
-    setTimeout(() => {
-      document.querySelector("#gameTimer").innerText =
-        Number(document.querySelector("#gameTimer").innerText) - 1;
+  function clearDots(ele) {
+    const deleteDotsInterval = setInterval(() => {
+      if (ele.parentNode) {
+        ele.parentNode.removeChild(ele);
+      }
+      clearInterval(deleteDotsInterval);
     }, dotTime);
-  }, 1000);
+  }
 
-  const dotSpawner = setInterval(() => {
+  function moreDots() {
+    for (let i = 0; i < 5; i++) {
+      let dot = document.createElement("div");
+      dot.style.position = "absolute";
+      dot.classList.add("dot");
+      let bottomPositon = Math.floor(
+        Math.min(
+          Math.random() *
+          document.querySelector("#background").getBoundingClientRect().bottom,
+          document.querySelector("#background").getBoundingClientRect().bottom -
+          175
+        )
+      );
+      let rightPosition = Math.min(
+        Math.random() *
+        document.querySelector("#background").getBoundingClientRect().right,
+        document.querySelector("#background").getBoundingClientRect().right - 50
+      );
+      dot.style.bottom = bottomPositon + "px";
+      dot.style.left = rightPosition + "px";
+      document
+        .querySelector("#background")
+        .insertAdjacentElement("afterbegin", dot);
+    }
+    document.querySelectorAll(".dot").forEach(ele => {
+      ele.addEventListener("click", e => {
+        e.target.parentNode.removeChild(e.target);
+      });
+      clearDots(ele);
+    });
+  }
+
+  function handleDotsOnInteraction(e) {
+    if (e.target.className === "dot") {
+      document.querySelector("#countClick").innerText = countClick += 1;
+    }
+  }
+
+  document.addEventListener("click", e => handleDotsOnInteraction(e));
+
+  function playGame() {
+    let mode = document.querySelectorAll("input[name=mode]:checked")[0];
     document.querySelector("#countDown").innerText = mode.value;
-    moreDots();
-    document.querySelector("#dotCount").innerText =
-      Number(document.querySelector("#dotCount").innerText) + 5;
-  }, dotTime);
+    console.log(mode.id);
+    dotTime = 3000;
 
-  const clearDotSpawner = setInterval(() => {
-    clearInterval(dotSpawner);
-    clearInterval(countDownTimer);
-    clearInterval(clearDotSpawner);
-  }, 60000);
-}
+    const countDownTimer = setInterval(() => {
+      document.querySelector("#countDown").innerText =
+        Number(document.querySelector("#countDown").innerText) - 1;
 
-document.querySelector("#startGame").addEventListener("click", e => {
-  e.target.parentNode.removeChild(e.target);
-  document.querySelector("#countDown").style.display = "block";
-  playGame();
+      setTimeout(() => {
+        document.querySelector("#gameTimer").innerText =
+          Number(document.querySelector("#gameTimer").innerText) - 1;
+      }, dotTime);
+    }, 1000);
+
+    const dotSpawner = setInterval(() => {
+      const dotCount = Number(document.querySelector("#dotCount").innerText)
+      document.querySelector("#countDown").innerText = mode.value;
+      moreDots();
+      document.querySelector("#dotCount").innerText = dotCount + 5;
+      if (dotCount === 95) {
+        const clearDotSpawner = setInterval(() => {
+          clearInterval(dotSpawner);
+          clearInterval(countDownTimer);
+          clearInterval(clearDotSpawner);
+        }, 0);
+        fetch("https://boiling-citadel-90849.herokuapp.com/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({username: username.value || "brian", score: Number(document.querySelector("#countClick").innerText)})
+        }).then(r => r.json())
+          .then(data => console.log("Success " + data))
+          .catch(error => console.log("Error " + error));
+      }
+    }, dotTime);
+  }
+
+  document.querySelector("#startGame").addEventListener("click", e => {
+    e.target.style.visibility = "hidden";
+    username.style.visibility = "hidden";
+    window.scrollTo(0, document.body.scrollHeight);
+    document.querySelector("#countDown").style.display = "block";
+    playGame();
+  });
 });
-
-/*** TODO's
-  1. Add Timer -- COMPLETED
-  2. Add Modes -- COMPLETED
-  3. Add scores in real time
-  3. Add Sounds
-***/
